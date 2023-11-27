32d6be681dfce5ca4cedc8943ce1feffb1fd7c14
andreeabejinariu0 <andreea.bejinariu@stud.ubbcluj.ro>
2023-11-27 17:40:03 +0200

    moved the modal in navbar and added search movies

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -10,23 +10,6 @@
         <div class="btns">
           <a class="btn-menu animated fadeInUp scrollto ">Explore our movies</a>
         </div> 
-        <!-- Modalul de Log in din NavBar-->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog  modal-dialog-centered ">
-            <div class="modal-content" style="  color: #E9CC6D; border: 2px solid #001540;">
-              <div class="modal-header">
-                <h2 class="modal-title fst-italic " id="exampleModalLabel" style=" color: #001540;">Log in</h2>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary" style="background-color: #001540; border: black;"><i
-                    class="bi bi-google"></i> Log in with Google account</button>
-              </div>
-            </div>
-          </div>
-        </div>
-
       </div>
 
     </div>
